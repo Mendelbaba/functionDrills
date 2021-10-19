35b2c54a1c91c68057550bbe6cc69d48e34bc599
Mendelbaba <mendybaba@gmail.com>
2021-10-18 20:50:24 -0400

    finished

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -260,12 +260,36 @@ function buysFrogs2(gold) {
 
 ////////////////// PROBLEM 15 ////////////////////
 let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+let sampleArray2 = [1,2,3,4,5,6]
 /*
-  Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
+  Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. 
+  The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function
+   invoked. Use the sample array to test this function.
 */
 
 //CODE HERE
-
+// function confirmAscending(arr) {
+  
+//   for (i = 0; i < arr.length; i++) {
+//     let j = i - 1;
+//     if (arr[i] > arr[j]) {
+//       return "true"
+//     } else {
+//       return "false"
+//     }
+//   }
+// } console.log(confirmAscending(sampleArray2))
+function confirmAscending(arr) {
+  let comparison = arr[0]
+  for (i = 1; i < arr.length -1 ; i++) {
+    if (arr[i] <= comparison) {
+      return false
+    } else {
+      comparison = arr[i]
+    } return true 
+  }
+}
+console.log(confirmAscending(sampleArray2))
 
 ////////////////// PROBLEM 16 ////////////////////
 
@@ -289,13 +313,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ["duck"]
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ["duck", "rubberDuck"]
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ["sailorDuck", "rubberDuck", "duck"]
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ["realDuck", "duck"]
